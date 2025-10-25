c10acbb7dcfd2e1db11a21d660ab93a966f6860b
falb <diegomassanova4@gmail.com>
2025-10-25 23:07:32 +0200

    add helper function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::io::Write;
+
+use curl::easy::Handler;
+
 pub mod color;
 pub mod image_comparison;
 pub mod image_data;
@@ -9,3 +13,21 @@ pub mod tile_coords;
 pub fn convert_px_to_hours(px: u32) -> f64 {
     (px as f64) / 120.0
 }
+
+struct GenericBytes(Vec<u8>);
+impl Handler for GenericBytes {
+    fn write(&mut self, data: &[u8]) -> Result<usize, curl::easy::WriteError> {
+        self.0
+            .write_all(data)
+            .map_err(|_| curl::easy::WriteError::Pause)?;
+        Ok(data.len())
+    }
+}
+
+// struct GenericBytesCursor<'a>(&'a mut std::io::Cursor<Vec<u8>>);
+// impl <'a>Handler for GenericBytesCursor<'a> {
+//     fn write(&mut self, data: &[u8]) -> Result<usize, curl::easy::WriteError> {
+//         self.0.write_all(data);
+//         Ok(data.len())
+//     }
+// }
