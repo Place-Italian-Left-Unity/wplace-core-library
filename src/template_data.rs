7afe49ae6542e72ff04557804041f46ebe04acef
falb <diegomassanova4@gmail.com>
2025-10-26 13:34:12 +0100

    cargo fmt

--- src/template_data.rs
+++ src/template_data.rs
@@ -11,7 +11,7 @@ pub struct TemplateData {
     top_left_corner: TileCoords,
     center_coordinates: MapCoords,
     image: ImageData,
-    file_name: String
+    file_name: String,
 }
 
 #[derive(Debug)]
@@ -19,7 +19,7 @@ pub enum TemplateDataError {
     IoError(std::io::Error),
     ImageDataError(ImageDataError),
     TileCoordsError(TileCoordsError),
-    NoFileName
+    NoFileName,
 }
 
 impl Display for TemplateDataError {
@@ -28,7 +28,7 @@ impl Display for TemplateDataError {
             Self::IoError(e) => write!(f, "IO Error: {e}"),
             Self::ImageDataError(e) => write!(f, "ImageData Error: {e}"),
             Self::TileCoordsError(e) => write!(f, "TileCoords Error: {e}"),
-            Self::NoFileName => write!(f, "No file name")
+            Self::NoFileName => write!(f, "No file name"),
         }
     }
 }
@@ -43,7 +43,11 @@ impl TemplateData {
         let top_left_corner = TileCoords::parse_tile_coords_string(top_left_corner_coords_str)
             .map_err(TemplateDataError::TileCoordsError)?;
         let file_path = file_path.as_ref();
-        let file_name = file_path.file_name().ok_or(TemplateDataError::NoFileName)?.to_str().ok_or(TemplateDataError::NoFileName)?;
+        let file_name = file_path
+            .file_name()
+            .ok_or(TemplateDataError::NoFileName)?
+            .to_str()
+            .ok_or(TemplateDataError::NoFileName)?;
         let image = ImageData::new(
             std::fs::read(file_path)
                 .map_err(TemplateDataError::IoError)?
@@ -53,7 +57,12 @@ impl TemplateData {
         Ok(Self::new(name, top_left_corner, file_name, image))
     }
 
-    pub fn new(name: impl ToString, top_left_corner: TileCoords, file_name: impl ToString, image: ImageData) -> Self {
+    pub fn new(
+        name: impl ToString,
+        top_left_corner: TileCoords,
+        file_name: impl ToString,
+        image: ImageData,
+    ) -> Self {
         Self {
             name: name.to_string(),
             center_coordinates: MapCoords::from_tile_coords(
@@ -92,7 +101,7 @@ impl TemplateData {
     pub fn get_image(&self) -> &ImageData {
         &self.image
     }
-    
+
     pub fn get_file_name(&self) -> &str {
         &self.file_name
     }
