6bd9fb811f910524c219c2d4db113cf07b55f7d6
falb <diegomassanova4@gmail.com>
2025-11-10 16:36:04 +0100

    faster algorithm for tile coords

--- src/tile_coords.rs
+++ src/tile_coords.rs
@@ -7,25 +7,12 @@ pub struct TileCoords {
 
 #[derive(thiserror::Error, Debug)]
 pub enum TileCoordsError {
-    #[error("Strip Suffix")]
-    StripSuffix,
-    #[error("Strip Prefix")]
-    StripPrefix,
-    #[error("Deformed TileCoords")]
-    DeformedTileCoords,
     #[error("Parse Error: {0}")]
     ParseError(#[from] std::num::ParseIntError),
-    #[error("No Tile X Coords")]
-    NoTileXCoords,
-    #[error("No Tile Y Coords")]
-    NoTileYCoords,
-    #[error("No X Coords")]
-    NoXCoords,
-    #[error("No Y Coords")]
-    NoYCoords,
 }
 
 impl TileCoords {
+    #[inline(always)]
     pub fn new(tile_x: u16, tile_y: u16, x: u16, y: u16) -> Self {
         TileCoords {
             tile_x,
@@ -36,28 +23,42 @@ impl TileCoords {
     }
 
     pub fn parse_tile_coords_string(v: &str) -> Result<Self, TileCoordsError> {
-        let mut tile_coords = v
-            .trim()
-            .strip_prefix('(')
-            .ok_or(TileCoordsError::StripPrefix)?
-            .strip_suffix(')')
-            .ok_or(TileCoordsError::StripSuffix)?
-            .split(',')
-            .map(|x| {
-                x.split(':')
-                    .next_back()
-                    .ok_or(TileCoordsError::DeformedTileCoords)?
-                    .trim()
-                    .parse::<u16>()
-                    .map_err(TileCoordsError::ParseError)
-            });
-
-        Ok(Self {
-            tile_x: tile_coords.next().ok_or(TileCoordsError::NoTileXCoords)??,
-            tile_y: tile_coords.next().ok_or(TileCoordsError::NoTileYCoords)??,
-            x: tile_coords.next().ok_or(TileCoordsError::NoXCoords)??,
-            y: tile_coords.next().ok_or(TileCoordsError::NoYCoords)??,
-        })
+        let mut buff = String::with_capacity(4);
+        
+        let mut out = Self::new(0, 0, 0, 0);
+        
+        let mut is_writing = false;
+        let mut last_character = ' ';
+        let mut counter = 0;
+        for character in v.chars() {
+            if last_character == ':' && character == ' ' {
+                is_writing = true;
+                counter += 1;
+                continue;
+            } else if character == ',' || character == ')' {
+                is_writing = false;
+                match counter {
+                    1 => out.x = buff.parse()?,
+                    2 => out.y = buff.parse()?,
+                    3 => out.tile_x = buff.parse()?,
+                    4 => { out.tile_y = buff.parse()?; break; },
+                    _ => unreachable!()
+                }
+                buff.clear();
+                continue;
+            };
+            
+            if is_writing {
+                match counter {
+                    1..=4 => buff.push(character),
+                    _ => unreachable!()
+                }
+            }
+            
+            last_character = character;
+        }
+        
+        Ok(out)
     }
 
     pub fn get_x(&self) -> u16 {
